d27de0a5d758092cd01219e14188b878691a8f0f
Raul Sevilla <rsevilla@redhat.com>
2020-11-13 13:58:59 +0100

    Generate job summary document
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

---
+++ pkg/burner/metadata.go
@@ -0,0 +1,46 @@
+// Copyright 2020 The Kube-burner Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package burner
+
+import (
+	"time"
+
+	"github.com/cloud-bulldozer/kube-burner/log"
+	"github.com/cloud-bulldozer/kube-burner/pkg/config"
+	"github.com/cloud-bulldozer/kube-burner/pkg/indexers"
+)
+
+type metadata struct {
+	Timestamp   time.Time  `json:"timestamp"`
+	UUID        string     `json:"uuid"`
+	MetricName  string     `json:"metricName"`
+	ElapsedTime float64    `json:"elapsedTime"`
+	JobConfig   config.Job `json:"jobConfig"`
+}
+
+// IndexMetadataInfo Generates and indexes a document with metadata information of the passed job
+func IndexMetadataInfo(indexer *indexers.Indexer, uuid string, elapsedTime float64, jobConfig config.Job) {
+	metadataInfo := []interface{}{
+		metadata{
+			UUID:        uuid,
+			ElapsedTime: elapsedTime,
+			JobConfig:   jobConfig,
+			MetricName:  "jobSummary",
+			Timestamp:   time.Now(),
+		},
+	}
+	log.Infof("Indexing metadata information: %+v", metadataInfo)
+	(*indexer).Index(config.ConfigSpec.GlobalConfig.IndexerConfig.DefaultIndex, metadataInfo)
+}
