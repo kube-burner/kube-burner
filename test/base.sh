9ae0f461871645818b3fa9331931698c276c8f6d
Raul Sevilla <rsevilla@redhat.com>
2021-07-01 10:31:02 +0200

    Print events on error
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- test/base.sh
+++ test/base.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+trap print_events ERR
 export QPS=5
 export BURST=10
 export TERM=screen-256color
@@ -10,4 +11,8 @@ normal=$(tput sgr0)
 
 log() {
     echo ${bold}$(date "+%d-%m-%YT%H:%M:%S") ${@}${normal}
+}
+
+print_events () {
+  kubectl get events --sort-by='.lastTimestamp' -A
 }
