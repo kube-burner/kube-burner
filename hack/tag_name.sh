8a2b4a5826f7f493bcffa126c20848d5cfca9f00
Raul Sevilla <rsevilla@redhat.com>
2021-08-08 22:04:53 +0200

    Fix CI for pull_request event
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- hack/tag_name.sh
+++ hack/tag_name.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [[ -z $(git branch --show-current) ]]; then
-  git describe --tags
+  git describe --tags --abbrev=0
 else
   git branch --show-current | sed 's/master/latest/g'
 fi
