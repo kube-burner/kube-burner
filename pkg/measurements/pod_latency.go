d7dbd45c22fd2e4469e65a35820416d1b3a6f4c8
Raul Sevilla <rsevilla@redhat.com>
2022-09-09 10:37:45 +0200

    Reset latency slices when stopping measurement
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- pkg/measurements/pod_latency.go
+++ pkg/measurements/pod_latency.go
@@ -161,6 +161,9 @@ func (p *podLatency) stop() (int, error) {
 		pq := q.(metrics.LatencyQuantiles)
 		log.Infof("%s: %s 50th: %v 99th: %v max: %v avg: %v", factory.jobConfig.Name, pq.QuantileName, pq.P50, pq.P99, pq.Max, pq.Avg)
 	}
+	// Reset latency slices, required in multi-job benchmarks
+	p.latencyQuantiles = nil
+	p.normLatencies = nil
 	return rc, nil
 }
 
