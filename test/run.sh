619873ccef51b3858102ca9d059b67b98196b381
dry923 <rzaleski@redhat.com>
2022-10-04 07:41:50 -0400

    Added churn to namespaced test case

--- test/run.sh
+++ test/run.sh
@@ -49,7 +49,7 @@ check_files () {
 }
 
 log "Running kube-burner init"
-timeout 300 kube-burner init -c kube-burner.yml --uuid ${uuid} --log-level=debug -u http://localhost:9090 -m metrics-profile.yaml -a alert-profile.yaml
+timeout 500 kube-burner init -c kube-burner.yml --uuid ${uuid} --log-level=debug -u http://localhost:9090 -m metrics-profile.yaml -a alert-profile.yaml
 check_files
 check_ns kube-burner-job=namespaced,kube-burner-uuid=${uuid} 10
 check_destroyed_ns kube-burner-job=not-namespaced,kube-burner-uuid=${uuid}
