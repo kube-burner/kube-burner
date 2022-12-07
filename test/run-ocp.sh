1ea26172b97fdd98c012dce6cb3b8f1915f10fed
Raul Sevilla <rsevilla@redhat.com>
2022-12-07 01:05:55 +0100

    Fix QPS in non-optimal scenarios
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- test/run-ocp.sh
+++ test/run-ocp.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 trap 'cleanup' ERR
 
 cleanup() {
-  oc delete ns -l kube-burner-uuid=${UUID} 
+  oc delete ns -l kube-burner-uuid=${UUID} --ignore-not-found
+  exit 1
 }
 
 UUID=$(uuidgen)
 ES_SERVER="https://search-perfscale-dev-chmf5l4sh66lvxbnadi4bznl3a.us-west-2.es.amazonaws.com/"
 ES_INDEX="kube-burner-ocp"
-COMMON_FLAGS="--es-server=${ES_SERVER} --es-index=${ES_INDEX} --alerting=false --uuid=${UUID}"
+COMMON_FLAGS="--es-server=${ES_SERVER} --es-index=${ES_INDEX} --alerting=false --uuid=${UUID} --qps=5 --burst=5"
 
 echo "Running node-density wrapper"
 kube-burner ocp node-density --pods-per-node=75 --pod-ready-threshold=10s --container-image=gcr.io/google_containers/pause:3.0 ${COMMON_FLAGS}
