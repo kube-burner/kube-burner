a081091b924fb92392e98b3b3a7f4e62bd8213b6
7908837174 <7908837174@github.com>
2025-07-18 18:25:58 +0530

    Simplify netcat command for busybox compatibility
    - Use echo pipe to nc instead of complex command
    - Remove problematic flags and redirections
    - Should work with busybox netcat implementation
    - Addresses service checker verification failure
    
    Signed-off-by: 7908837174 <7908837174@github.com>

--- pkg/measurements/util/svc_checker.go
+++ pkg/measurements/util/svc_checker.go
@@ -39,8 +39,8 @@ func (lc *SvcLatencyChecker) Ping(address string, port int32, timeout time.Durat
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	// We use 50ms precision thanks to sleep 0.05
-	// Use simple nc command compatible with busybox
-	cmd := []string{"sh", "-c", fmt.Sprintf("while true; do nc %s %d </dev/null && break; sleep 0.05; done", address, port)}
+	// Use simple echo to nc pipe that works with busybox
+	cmd := []string{"sh", "-c", fmt.Sprintf("while true; do echo '' | nc %s %d && break; sleep 0.05; done", address, port)}
 	req := lc.clientSet.CoreV1().RESTClient().Post().
 		Resource("pods").
 		Name(lc.Pod.Name).
