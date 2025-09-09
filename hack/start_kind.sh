666cffd2185931a35cdea154a282a0bc430b673b
Ygal Blum <ygal.blum@gmail.com>
2025-09-09 10:06:33 -0400

    Fix shellcheck lints
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- hack/start_kind.sh
+++ hack/start_kind.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-TOP_DIR=$(dirname $(dirname $(realpath $0)))
+TOP_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 
 ARCH=$(uname -m | sed s/aarch64/arm64/ | sed s/x86_64/amd64/)
-KIND_FOLDER=${KIND_FOLDER:-$(mktemp -d)}
-KIND_YAML=${TOP_DIR}/test/k8s/kind.yml
-KUBEVIRT_CR=${TOP_DIR}/test/k8s/objectTemplates/kubevirt-cr.yaml
-source ${TOP_DIR}/test/helpers.bash
+export KIND_FOLDER=${KIND_FOLDER:-$(mktemp -d)}
+export KIND_YAML=${TOP_DIR}/test/k8s/kind.yml
+export KUBEVIRT_CR=${TOP_DIR}/test/k8s/objectTemplates/kubevirt-cr.yaml
+source "${TOP_DIR}"/test/helpers.bash
 
 setup-kind
 
