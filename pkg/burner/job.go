215b56be2c644cfaf443d35ebb53006e3dbcac88
Marius Cornea <mcornea@redhat.com>
2026-03-03 17:39:41 +0200

    Add churn deleteDelay option (#1177)
    Introduce a configurable delay between object deletion and recreation
    during churn cycles. Defaults to 0s.
    
    ## Type of change
    
    - New feature 
    
    ## Description
    
    Add a `deleteDelay` option to `ChurnConfig` that introduces a
    configurable pause after object deletion and before recreation within a
    churn period. This applies to both `namespaces` and `objects` churn
    modes. The option defaults to `0s`, preserving existing behavior.
    
    For autoscaler tests, a delay after deleting objects is needed to allow
    the autoscaler to remove nodes during churn. The existing `delay` option
    happens after the complete churn cycle (deletion + recreation) so it
    doesn't help for this use case.
    
    Signed-off-by: Marius Cornea <mcornea@redhat.com>
    Co-authored-by: Raúl Sevilla <rsevilla@redhat.com>

--- pkg/burner/job.go
+++ pkg/burner/job.go
@@ -124,6 +124,7 @@ func Run(configSpec config.Spec, kubeClientProvider *config.KubeClientProvider, 
 					log.Infof("Churn duration: %v", jobExecutor.ChurnConfig.Duration)
 					log.Infof("Churn percent: %v", jobExecutor.ChurnConfig.Percent)
 					log.Infof("Churn delay: %v", jobExecutor.ChurnConfig.Delay)
+					log.Infof("Churn delete delay: %v", jobExecutor.ChurnConfig.DeleteDelay)
 					log.Infof("Churn type: %v", jobExecutor.ChurnConfig.Mode)
 				}
 				if jobErrs := jobExecutor.RunCreateJob(ctx, 0, jobExecutor.JobIterations); jobErrs != nil {
