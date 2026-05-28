c301855392c6561fd12c1179dbb90982fd6aaa44
Raul Sevilla <rsevilla@redhat.com>
2026-05-28 11:39:55 +0200

    Remove authorization faltal error
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- pkg/burner/create.go
+++ pkg/burner/create.go
@@ -338,10 +338,7 @@ func (ex *JobExecutor) createRequest(ctx context.Context, gvr schema.GroupVersio
 			}
 		}
 		if err != nil {
-			if kerrors.IsUnauthorized(err) {
-				log.Fatalf("Authorization error creating %s/%s: %s", obj.GetKind(), obj.GetName(), err)
-				return true, err
-			} else if kerrors.IsAlreadyExists(err) {
+			if kerrors.IsAlreadyExists(err) {
 				if ns != "" {
 					log.Errorf("%s/%s in namespace %s already exists", obj.GetKind(), obj.GetName(), ns)
 				} else {
